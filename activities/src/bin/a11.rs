e40a7c53762fe7c3ff912920e624b3e669b91441
Scott Mathis <mathisscott@outlook.com>
2022-02-04 17:32:30 -0500

    act 11

--- activities/src/bin/a11.rs
+++ activities/src/bin/a11.rs
@@ -9,4 +9,25 @@
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
 
-fn main() {}
+struct Item {
+    quantity: i32,
+    id: i32
+}
+
+fn item_quantity(item: &Item) {
+    println!("There are {:?} items", item.quantity);
+}
+
+fn item_id(item: &Item) {
+    println!("The item id is {:?}", item.id);
+}
+
+fn main() {
+    let my_item = Item {
+        quantity: 100,
+        id: 2048
+    };
+
+    item_quantity(&my_item);
+    item_id(&my_item);
+}
