014781eaa3a371403d777f74b80061a1aefc6e55
Scott Mathis <mathisscott@outlook.com>
2022-02-04 14:37:22 -0500

    act 3

--- activities/src/bin/a3b.rs
+++ activities/src/bin/a3b.rs
@@ -9,4 +9,19 @@
 // * Use an if..else if..else block to determine which message to display
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn relation_to_five(num: i32) -> &'static str {
+    let return_str;
+    if num > 5 {
+        return_str = ">5";
+    } else if num < 5 {
+        return_str = "<5";
+    } else {
+        return_str = "=5";
+    }
+    return_str
+}
+
+fn main() {
+    let test = 6;
+    println!("{} is {}", test, relation_to_five(test));
+}
