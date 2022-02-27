df1b42f8186f1c9331807042ba3eb82db59c2620
Scott Mathis <mathisscott@outlook.com>
2022-02-27 13:36:51 -0500

    more acts

--- activities/src/bin/a38.rs
+++ activities/src/bin/a38.rs
@@ -8,6 +8,8 @@
 // Notes:
 // * Use the join function to wait for threads to finish
 
+use std::thread;
+
 fn msg_hello() -> &'static str {
     use std::time::Duration;
     std::thread::sleep(Duration::from_millis(1000));
@@ -26,4 +28,15 @@ fn msg_excited() -> &'static str {
     "!"
 }
 
-fn main() {}
+fn main() {
+    let msg_one = thread::spawn(move || msg_hello());
+    let msg_two = thread::spawn(move || msg_thread());
+    let msg_three = thread::spawn(move || msg_excited());
+
+    let a = msg_one.join().expect("failed to join msg_hello");
+    let b = msg_two.join().expect("failed to join msg_thread");
+    let c = msg_three.join().expect("failed to join msg_excited");
+
+    println!("{}{}{}", a, b, c);
+    
+}
