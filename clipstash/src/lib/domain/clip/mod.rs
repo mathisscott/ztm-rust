6122d22f54e69dbab60e97c4356ca3647f32cb13
Scott Mathis <mathisscott@outlook.com>
2022-05-17 09:04:39 -0400

    proj 1

--- clipstash/src/lib/domain/clip/mod.rs
+++ clipstash/src/lib/domain/clip/mod.rs
@@ -1,1 +1,13 @@
 pub mod field;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Clip {
+    pub clip_id: field::ClipId,
+    pub shortcode: field::Shortcode,
+    pub content: field::Content,
+    pub title: field::Title,
+    pub posted: field::Posted,
+    pub expires: field::Expires,
+    pub password: field::Password,
+    pub hits: field::Hits,
+}
