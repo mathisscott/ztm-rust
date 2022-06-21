fda29e8125d7ccc98b994041996b65c18ab25459
Scott Mathis <mathisscott@outlook.com>
2022-06-21 13:14:57 -0400

    proj jun

--- clipstash/src/lib/data/mod.rs
+++ clipstash/src/lib/data/mod.rs
@@ -1,7 +1,47 @@
+pub mod model;
+pub mod query;
+
 use derive_more::{Display, From};
 use serde::{Deserialize, Serialize};
+use sqlx::Sqlite;
 use uuid::Uuid;
 use std::str::FromStr;
+
+#[derive(Debug, thiserror::Error)]
+pub enum DataError {
+    #[error("database error: {0}")]
+    Database(#[from] sqlx::Error)
+}
+
+// setting these types here allow us to change the database w/o affecting the app at large
+pub type AppDatabase = Database<Sqlite>;
+pub type DatabasePool = sqlx::sqlite::SqlitePool; // pool of connections that are constantly reused
+pub type Transaction<'t> = sqlx::Transaction<'t, Sqlite>; // allows us to rollback if there are any issues
+pub type AppDatabaseRow = sqlx::sqlite::SqliteRow;
+pub type AppQueryResult = sqlx::sqlite::SqliteQueryResult;
+
+pub struct Database<D: sqlx::Database>(sqlx::Pool<D>);
+
+impl Database<Sqlite> {
+    pub async fn new(connection_str: &str) -> Self {
+        let pool = sqlx::sqlite::SqlitePoolOptions::new()
+            .connect(connection_str)
+            .await;
+        
+        match pool {
+            Ok(pool) => Self(pool),
+            Err(e) => {
+                eprintln!("{}\n", e);
+                eprintln!("If the database has not been created, run: \n $ sqlx database setup \n");
+                panic!("database connection error");
+            }
+        }
+    }
+
+    pub fn get_pool(&self) -> &DatabasePool {
+        &self.0
+    }
+}
 
 #[derive(Clone, Debug, From, Display, Deserialize, Serialize)]
 pub struct DbId(Uuid);
